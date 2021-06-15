2e739c7d243299c14cbb0976826191f134aa990c
Rebecca Diroll <rebecca.diroll@gmail.com>
2021-06-15 12:27:35 -0400

    Remove React logo file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+// import React from "react";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
