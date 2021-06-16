e2fd4895bee8d536c0fd223ef29b15789f60dfe1
Rebecca Diroll <rebecca.diroll@gmail.com>
2021-06-16 19:16:42 -0400

    Import resume and add resume route in app.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from "./components/Footer/Footer.js"
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home/Home.js";
 import Work from "./pages/Work/Work.js";
+import Resume from "./pages/Resume/Resume.js";
 
 function App() {
   return (
@@ -15,8 +16,8 @@ function App() {
         <NavTabs></NavTabs>
         <Route exact path="/" component={Home} />
         <Route exact path="/work" component={Work} />
-        {/* <Route exact path="/contact" component={Contact} />
-        // <Route exact path="/resume" component={Resume} /> */}
+        {/* <Route exact path="/contact" component={Contact} /> */}
+        <Route exact path="/resume" component={Resume} />
         <Footer></Footer>
       </Router>
 
---
+++ src/pages/Resume/Resume.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Resume() {
+    return (
+        <>
+            <h1>
+                Resume
+            </h1>
+            <h2>
+                Summary
+            </h2>
+            <p>
+            Software Engineer with experience developing front-end and back-end web applications. 
+            Technical experience with hardware design and testing. Leadership skills including 
+            team leader, teacher mentor, teacher trainer, and assistant logistic coordinator.
+            </p>
+            <p>
+
+            </p>
+        </>
+    )
+}
+
+export default Resume;
