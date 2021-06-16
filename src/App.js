0c398efd7e16bb1e7cca8710ab03d7d0f46c50a0
Rebecca Diroll <rebecca.diroll@gmail.com>
2021-06-16 12:05:08 -0400

    Import Work page and add work route to App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import NavTabs from "./components/NavTabs/NavTabs";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home/Home.js";
+import Work from "./pages/Work/Work.js";
 
 function App() {
   return (
@@ -12,8 +13,8 @@ function App() {
         <br></br>
         <NavTabs></NavTabs>
         <Route exact path="/" component={Home} />
-        {/* <Route exact path="/projects" component={Projects} />
-        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/work" component={Work} />
+        {/* <Route exact path="/contact" component={Contact} />
         // <Route exact path="/resume" component={Resume} /> */}
         <footer></footer>
       </Router>
