6725c78dac1009dbc01277ea1fc3a3588280953b
Rebecca Diroll <rebecca.diroll@gmail.com>
2021-06-16 12:07:29 -0400

    Add Work page and placeholder text

---
+++ src/pages/Work/Work.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Work() {
+    return (
+        <>
+            <h1>
+                Work
+            </h1>
+            <h2>
+                Project 1
+            </h2>
+            <p>
+                Project 1 Description
+            </p>
+            <p>
+
+            </p>
+        </>
+    )
+}
+
+export default Work;
