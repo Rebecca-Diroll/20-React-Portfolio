3b123e1287586ce07a1e34c6b3a70f0288a004e1
Rebecca Diroll <rebecca.diroll@gmail.com>
2021-06-16 19:48:28 -0400

    Add header, summary, and technical skills to resume page

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -4,19 +4,43 @@ function Resume() {
     return (
         <>
             <h1>
-                Resume
+                RESUME
             </h1>
+            <div>
+                32019 Beech St., Hanoverton, OH 44423
+            </div>
+            <div>
+                Cell: 330-341-0602  |  Email: rebecca.diroll@gmail.com
+            </div>
+            <div>
+                LinkedIn: https://www.linkedin.com/in/rebecca-diroll-134a44102
+            </div>
+            <div>
+                GitHub: https://github.com/Rebecca-Diroll
+            </div>
             <h2>
-                Summary
+                SUMMARY
             </h2>
-            <p>
+            <div>
             Software Engineer with experience developing front-end and back-end web applications. 
             Technical experience with hardware design and testing. Leadership skills including 
             team leader, teacher mentor, teacher trainer, and assistant logistic coordinator.
-            </p>
-            <p>
-
-            </p>
+            </div>
+            <h2>
+                TECHNICAL SKILLS
+            </h2>
+            <div>
+                Languages: HTML, CSS, JavaScript
+            </div>
+            <div>
+                Technologies: React, Node.js, MongoDB, Express, REST, JSON, API, MySQL, Sequelize, BCrypt
+            </div>
+            <div>
+                Development: Responsive Design, Agile Software Development, Database Management, MVC
+            </div>
+            <div>    
+                Soft Skills: Detail Oriented, Collaboration, Communication, and Interpersonal Skills
+            </div>
         </>
     )
 }
