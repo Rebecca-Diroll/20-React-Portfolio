470dc046547ee1f6b1b51e6e966e940b1e6f1ca0
Rebecca Diroll <rebecca.diroll@gmail.com>
2021-06-16 13:52:46 -0400

    Import Footer component and revise spelling in the App function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './App.css';
-import NavTabs from "./components/NavTabs/NavTabs";
+import NavTabs from "./components/NavTabs/NavTabs.js";
+import Footer from "./components/Footer/Footer.js"
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home/Home.js";
 import Work from "./pages/Work/Work.js";
@@ -16,7 +17,7 @@ function App() {
         <Route exact path="/work" component={Work} />
         {/* <Route exact path="/contact" component={Contact} />
         // <Route exact path="/resume" component={Resume} /> */}
-        <footer></footer>
+        <Footer></Footer>
       </Router>
 
     </>
