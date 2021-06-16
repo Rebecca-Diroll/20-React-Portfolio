661b92b537665c3111b49c075510a2098cb8c028
Rebecca Diroll <rebecca.diroll@gmail.com>
2021-06-16 11:31:52 -0400

    Add About Me text to Home.js page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,8 +4,29 @@ function Home() {
     return (
         <>
             <h1>
-                Home Page!
+                Software Engineer
             </h1>
+            <h2>
+                About Me
+            </h2>
+            <p>
+                Software Engineer. Full Stack Web Development Bootcamp Certificate 
+                from The Ohio State University.
+            </p>
+            <p>
+                Four years experience as an Electrical Design Engineer at Motorola. 
+                Bachelor of Science in Engineering. EIT Certificate.
+            </p>
+            <p>
+                Teacher to university, high school, and middle school students.
+                Team Leader, Teacher Mentor, and Teacher Trainer. Attention to detail,
+                creative, and a team player.
+            </p>
+            <p>
+                Technologies include: HTML, CSS, JavaScript, JQuery, JSON, API, Node.js,
+                Express.js, MySQL, Sequelize, Handlebars, OOP, DotEnv, BCrypt, MVC, React,
+                GitHub, Heroku, Agile Software Development.
+            </p>
         </>
     )
 }
