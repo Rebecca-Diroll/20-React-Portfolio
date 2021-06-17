25c5bc7f30a2e7c4e835cf0d170aa735329da2d5
Rebecca Diroll <rebecca.diroll@gmail.com>
2021-06-17 16:04:59 -0400

    Rename constants in component Project.js

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,16 +1,51 @@
 import React from "react";
 
-function Project(props) {
-    console.log(props);
+function Project() {
+    const projectArray = [
+        {
+            projectTitle: "Project 1",
+            projectLink: "Link 1",
+            projectImage: "Image 1",
+            projectDesc: "Description 1"
+        },
+        {
+            projectTitle: "Project 2",
+            projectLink: "Link 2",
+            projectImage: "Image 2",
+            projectDesc: "Description 2"
+        },
+        {
+            projectTitle: "Project 3",
+            projectLink: "Link 3",
+            projectImage: "Image 3",
+            projectDesc: "Description 3"
+        },
+        {
+            projectTitle: "Project 4",
+            projectLink: "Link 4",
+            projectImage: "Image 4",
+            projectDesc: "Description 4"
+        },
+        {
+            projectTitle: "Project 5",
+            projectLink: "Link 5",
+            projectImage: "Image 5",
+            projectDesc: "Description 5"
+        },
+        {
+            projectTitle: "Project 6",
+            projectLink: "Link 6",
+            projectImage: "Image 6",
+            projectDesc: "Description 6"
+        }
+    ]
+
     return (
-        <>
-            <h1>Projects</h1>
-            <h2>props.projectName</h2>
-            <p>props.projectDesc</p>
-            <img>props.projectImage</img>
-            <p>props.projectLink</p>
-        </>
+        <div id="project-container">
+
+        </div>
     )
+
 }
 
 export default Project;
