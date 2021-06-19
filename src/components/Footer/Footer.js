59db3f56e5533f694a7c8690a9ed4064e87adb67
Rebecca Diroll <rebecca.diroll@gmail.com>
2021-06-18 21:56:58 -0400

    Format footer buttons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,17 @@
 import React from "react";
 // import { Link } from "react-router-dom";
-// import './NavTabs.css';
+import './Footer.css';
 
 function Footer() {
     return (
-        <navbar>
-            <a href="https://docs.google.com/document/d/1bv2vLVlDrhb30TA34iLhZXJHCJm6ScTH6qgDYrLBBW8/edit?usp=sharing" target="_blank" rel="noopener noreferrer"><button>Resume</button></a>
-            <a href="https://github.com/Rebecca-Diroll" target="_blank" rel="noopener noreferrer"><button>GitHub</button></a>
-            <a href="https://www.linkedin.com/in/rebecca-diroll-134a44102/" target="_blank" rel="noopener noreferrer"><button>LinkedIn</button></a>
-            <a href="mailto:rebecca.diroll@gmail.com" target="_blank" rel="noopener noreferrer"><button>Email</button></a>
-        </navbar>
+        <div className="footer">
+            <navbar>
+                <a href="https://docs.google.com/document/d/1bv2vLVlDrhb30TA34iLhZXJHCJm6ScTH6qgDYrLBBW8/edit?usp=sharing" target="_blank" rel="noopener noreferrer" className="footerButton"><button>Resume</button></a>
+                <a href="https://github.com/Rebecca-Diroll" target="_blank" rel="noopener noreferrer" className="footerButton"><button>GitHub</button></a>
+                <a href="https://www.linkedin.com/in/rebecca-diroll-134a44102/" target="_blank" rel="noopener noreferrer" className="footerButton"><button>LinkedIn</button></a>
+                <a href="mailto:rebecca.diroll@gmail.com" target="_blank" rel="noopener noreferrer" className="footerButton"><button>Email</button></a>
+            </navbar>
+        </div>
     )
 }
 
