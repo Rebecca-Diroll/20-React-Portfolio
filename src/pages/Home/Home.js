0045337f6dfc52ba2b1be6a6db63fecf4d899de2
Rebecca Diroll <rebecca.diroll@gmail.com>
2021-06-18 21:44:08 -0400

    Center text and add margins to the text on the Home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './Home.css';
 
 function Home() {
     return (
@@ -9,24 +10,28 @@ function Home() {
             <h2>
                 About Me
             </h2>
-            <p>
+            <p className="homeText">
                 Software Engineer. Full Stack Web Development Bootcamp Certificate 
                 from The Ohio State University.
             </p>
-            <p>
+            <br />
+            <p className="homeText">
                 Four years experience as an Electrical Design Engineer at Motorola. 
                 Bachelor of Science in Engineering. EIT Certificate.
             </p>
-            <p>
+            <br />
+            <p className="homeText">
                 Teacher to university, high school, and middle school students.
                 Team Leader, Teacher Mentor, and Teacher Trainer. Attention to detail,
                 creative, and a team player.
             </p>
-            <p>
+            <br />
+            <p className="homeText">
                 Technologies include: HTML, CSS, JavaScript, JQuery, JSON, API, Node.js,
                 Express.js, MySQL, Sequelize, Handlebars, OOP, DotEnv, BCrypt, MVC, React,
                 GitHub, Heroku, Agile Software Development.
             </p>
+            <br />
         </>
     )
 }
