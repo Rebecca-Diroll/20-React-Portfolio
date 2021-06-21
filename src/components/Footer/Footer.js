00a3fbb1fb3374ffa3ca60c94b606b434957762d
Rebecca Diroll <rebecca.diroll@gmail.com>
2021-06-20 20:09:52 -0400

    Apply css grid layout to header and footer buttons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,12 +5,10 @@ import './Footer.css';
 function Footer() {
     return (
         <div className="footer">
-            <navbar>
-                <a href="https://docs.google.com/document/d/1bv2vLVlDrhb30TA34iLhZXJHCJm6ScTH6qgDYrLBBW8/edit?usp=sharing" target="_blank" rel="noopener noreferrer"><button className="footerButton">Resume</button></a>
-                <a href="https://github.com/Rebecca-Diroll" target="_blank" rel="noopener noreferrer"><button className="footerButton">GitHub</button></a>
-                <a href="https://www.linkedin.com/in/rebecca-diroll-134a44102/" target="_blank" rel="noopener noreferrer"><button className="footerButton">LinkedIn</button></a>
-                <a href="mailto:rebecca.diroll@gmail.com" target="_blank" rel="noopener noreferrer"><button  className="footerButton">Email</button></a>
-            </navbar>
+            <a href="https://docs.google.com/document/d/1bv2vLVlDrhb30TA34iLhZXJHCJm6ScTH6qgDYrLBBW8/edit?usp=sharing" target="_blank" rel="noopener noreferrer"><button className="footerButton">Resume</button></a>
+            <a href="https://github.com/Rebecca-Diroll" target="_blank" rel="noopener noreferrer"><button className="footerButton">GitHub</button></a>
+            <a href="https://www.linkedin.com/in/rebecca-diroll-134a44102/" target="_blank" rel="noopener noreferrer"><button className="footerButton">LinkedIn</button></a>
+            <a href="mailto:rebecca.diroll@gmail.com" target="_blank" rel="noopener noreferrer"><button  className="footerButton">Email</button></a>
         </div>
     )
 }
--- src/components/NavTabs/NavTabs.js
+++ src/components/NavTabs/NavTabs.js
@@ -4,11 +4,9 @@ import './NavTabs.css';
 
 function NavTabs() {
     return (
-      <div className="nav">
-        <navbar>
-          <Link className="button" to="/"><button className="headerButton">About Me</button></Link>
-          <Link to="/work"><button className="headerButton">Work</button></Link>
-        </navbar>
+      <div class="navBar">
+        <Link to="/"><button className="headerButton" id="hb1">About Me</button></Link>
+        <Link to="/work"><button className="headerButton" id="hb2">Work</button></Link>
       </div>
     )
 }
